8426388989dc4bc3d2f3b830c28c85bf60dc833b
dkingfaria <diegosantosfaria@hotmail.com>
2021-01-13 00:30:05 -0300

    Changing project name.

--- MenorNumero/Properties/AssemblyInfo.cs
+++ MenorNumero/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
-[assembly: AssemblyTitle("HelloWorld")]
+[assembly: AssemblyTitle("MenorNumero")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HelloWorld")]
+[assembly: AssemblyProduct("MenorNumero")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
